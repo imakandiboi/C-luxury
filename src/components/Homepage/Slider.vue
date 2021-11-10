c13d51a8216b787ac72b16fe9b7dffe26ab343c0
imakandiboi <imakandiboi@yahoo.com>
2021-11-10 06:33:38 -0500

    commit message for first staging

---
+++ src/components/Homepage/Slider.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <!-- slider -->
+
+    <b-carousel class="pt-5 slides" id="carousel-1" v-model="slide" :interval="4000" background="#ababab" img-width="1024" img-height="280" style="text-shadow: 1px 1px 2px #333" @sliding-start="onSlideStart" @sliding-end="onSlideEnd">
+      <!-- Text slides with image -->
+      <b-carousel-slide :img-src="image"></b-carousel-slide>
+      <b-carousel-slide :img-src="image2"></b-carousel-slide>
+      <b-carousel-slide :img-src="image"></b-carousel-slide>
+    </b-carousel>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Slider",
+  data() {
+    return {
+      image: require("@/assets/img/slider-11.png"),
+      image2: require("@/assets/img/slider-2.png"),
+    }
+  },
+}
+</script>
+
+<style scoped>
+.carousel-control-next,
+.carousel-control-prev {
+  font-size: 12px;
+}
+.img-fluid {
+  height: 580px;
+}
+</style>
---
+++ src/views/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <slider />
+  </div>
+</template>
+
+<script>
+import Slider from "@/components/Homepage/Slider.vue"
+export default {
+  components: {
+    Slider,
+  },
+}
+</script>
+
+<style scoped></style>
