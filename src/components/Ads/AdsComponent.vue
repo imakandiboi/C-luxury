ee243b81a2bfac94e80615cde6aee3c8d746934f
imakandiboi <imakandiboi@yahoo.com>
2021-11-20 11:00:31 -0500

    Update AdsComponent.vue

--- src/components/Ads/AdsComponent.vue
+++ src/components/Ads/AdsComponent.vue
@@ -1,12 +1,10 @@
 <template>
   <div>
     <div class="container ads-container">
-      <div class="d-flex align-items-center justify-content-center">
+      <div class="d-flex card-sec align-items-center justify-content-center">
         <div class="card border-success mb-3" style="max-width: 18rem, height: 38rem">
           <div class="card-header bg-warning fs-4 text-success border text-center">Get Offer %</div>
-          <div class="card-body p-0 text-dark" style="height: 26rem">
-            <img :src="image" style="width: 100% ,height:100%" />
-          </div>
+          <div class="card-body p-0 text-dark" style="height: 26rem"><img :src="image" style="width: 100%" height="455" /></div>
           <div class="card-footer border-success text-center bg-warning fs-4 text-success">Get Offer %</div>
         </div>
       </div>
@@ -28,5 +26,13 @@ export default {
 .card-body {
   /* padding: 1px !important; */
   font-family: 'Cinzel', serif;
+}
+@media screen and (max-width: 776px) {
+  .card-sec {
+    padding-top: 40px !important;
+  }
+  .card-body img {
+    height: 100%;
+  }
 }
 </style>
